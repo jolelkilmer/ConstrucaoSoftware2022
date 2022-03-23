a9c9a78c480f0c2507a945b96423ac244f6b9d7a
Jolel Kilmer <jolelkilmer@gmail.com>
2022-03-23 00:50:31 -0300

    Add files via upload

---
+++ Projeto/src/projeto/Projeto.java
@@ -0,0 +1,12 @@
+package projeto;
+
+public class Projeto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
