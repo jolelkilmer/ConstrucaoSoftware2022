382a5884c7f1039d89a83ccce3b8e6e856e38fc4
Henrique Negri <henriquenegri84@gmail.com>
2022-03-30 13:45:09 -0300

    Criando controller de nota

---
+++ src/main/java/controller/NotaController.java
@@ -0,0 +1,15 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class NotaController {
+	@Controller
+	public class RelatorioController {
+		@RequestMapping("/gerarNota")
+		public String gerarNota(){
+			return "gerarNota";
+		}
+
+	}
+}
