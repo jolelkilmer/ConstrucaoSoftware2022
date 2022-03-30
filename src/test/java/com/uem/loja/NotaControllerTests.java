1deb837418d874a6d1731ae1d694830dfd1ee834
Henrique Negri <henriquenegri84@gmail.com>
2022-03-30 13:46:40 -0300

    Criando classe de teste do controller de nota

---
+++ src/test/java/com/uem/loja/NotaControllerTests.java
@@ -0,0 +1,13 @@
+package com.uem.loja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class NotaControllerTests {
+
+	@Test
+	public void testGerarNota(){
+	}
+
+}
