805b6d4439f0c6443f318060085cb11a07824519
Filipe Moreno <y8.filipe@gmail.com>
2022-04-11 21:58:16 -0300

    Alteração na interface e edição/remoção de produtos

--- src/main/java/com/uem/loja/controllers/ProdutoController.java
+++ src/main/java/com/uem/loja/controllers/ProdutoController.java
@@ -1,11 +1,15 @@
 package com.uem.loja.controllers;
 
+import com.uem.loja.models.Cliente;
+import com.uem.loja.models.Funcionario;
 import com.uem.loja.models.Produto;
 import com.uem.loja.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -36,11 +40,19 @@ public class ProdutoController {
 		return mv;
 	}
 
-	@RequestMapping("/removerProduto")
+	@RequestMapping("/produtos/remover")
 	public String removerProduto(long id) {
 		Produto produto = produtoRepository.findById(id);
 		produtoRepository.delete(produto);
 		return "redirect:/produtos";
+	}
+
+	@GetMapping("/produtos/editar")
+	public ModelAndView editarProduto(@RequestParam long id) {
+		ModelAndView mav = new ModelAndView("produto/editarProduto");
+		Produto produto = produtoRepository.findById(id);
+		mav.addObject("produtos", produto);
+		return mav;
 	}
 
 }
---
+++ src/main/java/com/uem/loja/repository/FuncionarioRepository.java
@@ -0,0 +1,8 @@
+package com.uem.loja.repository;
+
+import com.uem.loja.models.Funcionario;
+import org.springframework.data.repository.CrudRepository;
+
+public interface FuncionarioRepository extends CrudRepository<Funcionario, String>{
+	Funcionario findByCodigo(long codigo);
+}
