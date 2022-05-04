ae6c6297a484ec9a0e8b6628c64f6a37eb192cd1
jofeju <jopferreira84@gmail.com>
2022-05-03 22:20:08 -0300

    Adição de relatórios para funcionário e fornecedor

--- src/main/java/com/uem/loja/controllers/RelatoriosController.java
+++ src/main/java/com/uem/loja/controllers/RelatoriosController.java
@@ -16,6 +16,8 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.uem.loja.models.Cliente;
 import com.uem.loja.models.Produto;
+import com.uem.loja.models.Funcionario;
+import com.uem.loja.models.Fornecedor;
 
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperCompileManager;
@@ -27,6 +29,8 @@ import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 
 import com.uem.loja.repository.ClienteRepository;
 import com.uem.loja.repository.ProdutoRepository;
+import com.uem.loja.repository.FuncionarioRepository;
+import com.uem.loja.repository.FornecedorRepository;
 
 @Controller
 public class RelatoriosController {
@@ -36,6 +40,12 @@ public class RelatoriosController {
 	
 	@Autowired
 	private ProdutoRepository produtoRepo;
+	
+	@Autowired
+	private FuncionarioRepository funcionarioRepo;
+	
+	@Autowired
+	private FornecedorRepository fornecedoresRepo;
 	
 	@RequestMapping("/relatorios")
 	public ModelAndView relatorios() {
@@ -82,7 +92,51 @@ public class RelatoriosController {
 		System.err.println(data);
 
 		HttpHeaders headers = new HttpHeaders();
-		headers.add("Content-Disposition", "inline; filename=relatorioClientes.pdf");
+		headers.add("Content-Disposition", "inline; filename=relatorioProdutos.pdf");
+
+		return ResponseEntity.ok().headers(headers).contentType(MediaType.APPLICATION_PDF).body(data);
+	}
+	
+	@GetMapping(value = "/relatorioFuncionarios", produces = MediaType.APPLICATION_PDF_VALUE)
+	public ResponseEntity<byte[]> downloadFuncionarios() throws JRException, IOException {
+
+		Iterable<Funcionario> funcionarios = funcionarioRepo.findAll();
+
+		JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource((Collection<?>) funcionarios);
+
+		JasperReport compileReport = JasperCompileManager
+				.compileReport(new FileInputStream("funcionariosRelatorio.jrxml"));
+
+		JasperPrint jasperPrint = JasperFillManager.fillReport(compileReport, null, dataSource);
+
+		byte data[] = JasperExportManager.exportReportToPdf(jasperPrint);
+
+		System.err.println(data);
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Content-Disposition", "inline; filename=relatorioFuncionarios.pdf");
+
+		return ResponseEntity.ok().headers(headers).contentType(MediaType.APPLICATION_PDF).body(data);
+	}
+	
+	@GetMapping(value = "/relatorioFornecedores", produces = MediaType.APPLICATION_PDF_VALUE)
+	public ResponseEntity<byte[]> downloadFornecedores() throws JRException, IOException {
+
+		Iterable<Fornecedor> fornecedores = fornecedoresRepo.findAll();
+
+		JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource((Collection<?>) fornecedores);
+
+		JasperReport compileReport = JasperCompileManager
+				.compileReport(new FileInputStream("fornecedoresRelatorio.jrxml"));
+
+		JasperPrint jasperPrint = JasperFillManager.fillReport(compileReport, null, dataSource);
+
+		byte data[] = JasperExportManager.exportReportToPdf(jasperPrint);
+
+		System.err.println(data);
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Content-Disposition", "inline; filename=relatorioFornecedores.pdf");
 
 		return ResponseEntity.ok().headers(headers).contentType(MediaType.APPLICATION_PDF).body(data);
 	}
